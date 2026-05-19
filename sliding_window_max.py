7f9f59551a19cc5d46b6a851572ce0c6cae812bf
Dawson McCall <dawsonmccall62@gmail.com>
2026-05-18 23:25:30 -0500

    Sliding Window Maximum deque solution

--- sliding_window_max.py
+++ sliding_window_max.py
@@ -1,14 +1,36 @@
+import collections
+
 def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
-    curr_window = [num for num in nums[:k]]
     res = []
+    # deque = double sided queue
+    # in our case it will be a monotonically (always) decreasing stack
+    queue = collections.deque()
+    l = 0
+    r = 0 
 
-    res.append(max(curr_window))
+    while r < len(nums):
+        # if q not empty and curr num is > last num in deque, then
+        # we can replace last num with curr num (and continue doing so)
+        # until no other nums in deque or other num(s) > curr num
+        while queue and nums[queue[-1]] < nums[r]:
+            queue.pop()
+        queue.append(r)
 
-    for r in range(k, len(nums)):
-        curr_window.append(nums[r])
-        curr_window = curr_window[1:]
-        res.append(max(curr_window))
+        # since we are looking at a sliding window, we must remove any
+        # out of bound indexes in deque, if l > first index in deque, first index
+        # in deque must be outside of window
+        if l > queue[0]:
+            # pops first num from deque
+            queue.popleft()
         
+        # if window size == to k, then left ptr must move forward and max value
+        # can be appended to res
+        if r - l + 1 == k:
+            res.append(nums[queue[0]])
+            l += 1
+
+        r += 1
+
     return res
 
 if __name__ == "__main__":
