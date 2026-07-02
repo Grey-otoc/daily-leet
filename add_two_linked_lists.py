04747f9a86bebafa4fde4b7717a19fe8f188b5e7
Dawson McCall <dawsonmccall62@gmail.com>
2026-07-02 18:15:56 +0100

    Add Two Numbers From a Linked List solution (single pass)

--- add_two_linked_lists.py
+++ add_two_linked_lists.py
@@ -6,50 +6,28 @@ class ListNode:
         self.next = next
 
 def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-    l1_vals = []
+    dummy_head = node = ListNode()
 
-    curr = l1
-    while curr:
-        l1_vals.append(curr.val)
-        curr = curr.next
+    carry = 0
+    while l1 or l2 or carry:
+        l1_val = l1.val if l1 else 0
+        l2_val = l2.val if l2 else 0
+        new_val = l1_val + l2_val + carry
+        carry = new_val // 10
+        new_val %= 10
+        node.next = ListNode(new_val)
 
-    multiplier = 1
-    l1_total = 0
-    for val in l1_vals:
-        l1_total += val * multiplier
-        multiplier *= 10
-        
-    l2_vals = []
-
-    curr = l2
-    while curr:
-        l2_vals.append(curr.val)
-        curr = curr.next
-
-    multiplier = 1
-    l2_total = 0
-    for val in l2_vals:
-        l2_total += val * multiplier
-        multiplier *= 10
-
-    total = l1_total + l2_total
-    new_head = node = ListNode(total % 10)
-    total //= 10
-    
-    while total > 0:
-        node.next = ListNode(total % 10)
-        total //= 10
+        l1 = l1.next if l1 else l1
+        l2 = l2.next if l2 else l2
         node = node.next
-    
-    return new_head
+
+    return dummy_head.next
     
 if __name__ == "__main__":
-    l1 = (ListNode(1, ListNode(2, ListNode(3))))
-    l2 = (ListNode(4, ListNode(5, ListNode(6))))
+    l1 = (ListNode(9))
+    l2 = (ListNode(9))
     new_head = addTwoNumbers(l1, l2)
     
     while new_head:
         print(new_head.val)
         new_head = new_head.next
-        
-    print(1 % 10, 1 // 10)
