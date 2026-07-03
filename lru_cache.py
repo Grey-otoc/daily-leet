461773557e330e9d2aa0399b84d0b502153c9720
Dawson McCall <dawsonmccall62@gmail.com>
2026-07-03 21:37:37 +0100

    LRU Cache solution (doubly linked list)

--- lru_cache.py
+++ lru_cache.py
@@ -1,25 +1,59 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
 
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove_node(self, node: Node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def move_node_front(self, node: Node):
+        prev = self.head
+        nxt = self.head.next
+        node.next = nxt
+        node.prev = prev
+        node.next.prev = node
+        self.head.next = node
+
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         
-        val = self.cache.pop(key)
-        self.cache[key] = val
-        return val
+        node = self.cache[key]
+        self.remove_node(node)
+        self.move_node_front(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
-        if len(self.cache) == self.capacity and key not in self.cache:
-            oldest_key = next(iter(self.cache))
-            self.cache.pop(oldest_key)
-            self.cache[key] = value
+        node = None
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove_node(node)
+            node.value = value
         else:
-            self.cache.pop(key, None)
-            self.cache[key] = value
+            node = Node(key, value)
+
+        self.move_node_front(node)
+        self.cache[key] = node
+        
+        if len(self.cache) > self.capacity:
+            lru_node = self.tail.prev
+            self.remove_node(lru_node)
+            del self.cache[lru_node.key]
 
 if __name__ == "__main__":
     lRUCache = LRUCache(2)
