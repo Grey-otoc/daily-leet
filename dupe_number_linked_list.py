31fdb1849d64fc4a293c225a31b81b0a1abae0dc
Dawson McCall <dawsonmccall62@gmail.com>
2026-07-03 13:16:59 +0100

    Find the Duplicate Number in Linked List Array O(1) unintuitive solution

--- dupe_number_linked_list.py
+++ dupe_number_linked_list.py
@@ -1,10 +1,20 @@
 def findDuplicate(nums: list[int]) -> int:
-    for num in nums:
-        idx = abs(num) - 1
-        if nums[idx] < 0:
-            return abs(num)
-        else:
-            nums[idx] *= -1
+    slow = fast = 0
+    
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        
+        if slow == fast:
+            break
+        
+    slow2 = 0
+    while True:
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        
+        if slow == slow2:
+            return slow
             
 if __name__ == "__main__":
     print(findDuplicate([1,2,3,6,4,2,5]))
