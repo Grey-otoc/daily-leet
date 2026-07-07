c094c08164ea229fec7c767bb9607cb2aff7aefa
Dawson McCall <dawsonmccall62@gmail.com>
2026-07-07 22:17:47 +0100

    Merge K Sorted Linked Lists more efficient

--- merge_k_linked_lists.py
+++ merge_k_linked_lists.py
@@ -4,32 +4,39 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
+        
 def mergeKLists(lists: list[Optional[ListNode]]) -> Optional[ListNode]:
     if not lists:
         return None
-    
-    for i in range(1, len(lists)):
-        list1 = lists[i]
-        list2 = lists[i-1]
 
-        head = tail = ListNode()
+    while len(lists) > 1:
+        mergedLists = []
+        for i in range(0, len(lists), 2):
+            list1 = lists[i]
+            list2 = lists[i+1] if (i + 1) < len(lists) else None
+            mergedList = mergeList(list1, list2)
+            mergedLists.append(mergedList)
 
-        while list1 and list2:
-            if list1.val <= list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
+        lists = mergedLists
 
-            tail = tail.next
+    return lists[0]
 
-        tail.next = list1 if list1 else list2
+def mergeList(list1: list[ListNode], list2: list[ListNode]) -> ListNode:
+    head = tail = ListNode()
 
-        lists[i] = head.next
+    while list1 and list2:
+        if list1.val <= list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
 
-    return lists[-1]
+        tail = tail.next
+
+    tail.next = list1 if list1 else list2
+
+    return head.next
     
 if __name__ == "__main__":
     list1 = ListNode(1, ListNode(2, ListNode(4)))
